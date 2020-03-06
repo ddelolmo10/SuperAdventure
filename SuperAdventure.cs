c93ad9f7a0e334f2f243e7df7bcedfdfe02beb59
David <ddelolmo1524@gmail.com>
2020-03-06 15:39:56 -0500

    initial commit

---
+++ SuperAdventure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Engine;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player player = new Player();
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            player.CurrentHitPoints = 10;
+            player.MaximumHitPoints = 10;
+            player.Gold = 20;
+            player.ExperiencePoints = 0;
+            player.Level = 1;
+
+            lblHitPoints.Text = player.CurrentHitPoints.ToString();
+            lblGold.Text = player.Gold.ToString();
+            lblExperience.Text = player.ExperiencePoints.ToString();
+            lblLevel.Text = player.Level.ToString();
+        }
+
+    }
+}
